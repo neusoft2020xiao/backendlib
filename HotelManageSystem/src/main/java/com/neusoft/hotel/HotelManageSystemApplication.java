9e90f0da999c0921b450b9bcfbf8c83b6f5f37d1
1170300508 <3246560862@qq.com>
2020-07-27 10:06:29 +0800

    001

---
+++ HotelManageSystem/src/main/java/com/neusoft/hotel/HotelManageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.neusoft.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManageSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManageSystemApplication.class, args);
+	}
+
+}
